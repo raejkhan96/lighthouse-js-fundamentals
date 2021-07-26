9a15d8c05c256ca46b78540e91c4724cfd17cb36
Rae J. Khan <raekhan18@gmail.com>
2021-07-26 17:44:34 +0000

    Hello exercise

--- hello.js
+++ hello.js
@@ -1,6 +1,14 @@
-const sayHello = function(name){
+/*const sayHello = function(name){
 	console.log("Hello, " + name);
 }
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+const returnSayHello = function (name){
+	return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
