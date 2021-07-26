5d72f48f0fec4a830a93dec9fec23f9d9c06ed1d
Rae J. Khan <raekhan18@gmail.com>
2021-07-26 18:58:00 +0000

    Find last index of number

---
+++ lastindexof.js
@@ -0,0 +1,19 @@
+function lastIndexOf(arr, num){
+  for(let i = arr.length-1; i > -1; i--){
+    if (arr[i] === num){
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+
